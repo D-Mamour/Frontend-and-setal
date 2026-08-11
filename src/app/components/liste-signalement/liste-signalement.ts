bf4965f059c7b3fd426ebec6840da09dc8556b34
M_DIONE <admin@192.168.1.45>
2026-08-11 18:56:27 +0000

    La creation de l'ensemble des grandes dossiers que l'ont aura besoin

---
+++ src/app/components/liste-signalement/liste-signalement.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-signalement',
+  imports: [],
+  templateUrl: './liste-signalement.html',
+  styleUrl: './liste-signalement.css',
+})
+export class ListeSignalement {}
