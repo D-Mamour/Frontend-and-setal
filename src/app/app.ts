8418827f47bae1a377dc0a9899dcdd98d5572d50
M_DIONE <admin@192.168.1.13>
2026-08-12 15:31:42 +0000

    l'integration des signalements avec la representation graphique

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ListeSignalement } from './components/liste-signalement/liste-signalement';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ ListeSignalement],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
