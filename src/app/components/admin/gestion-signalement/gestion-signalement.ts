dd0a6d6af9b355ba6a10ab61ebb6799890d329a6
BigLineDev0 <alioudiallo.py@gmail.com>
2026-08-18 15:30:43 +0000

    implementation gestion signalement espace admin

--- src/app/components/admin/gestion-signalement/gestion-signalement.ts
+++ src/app/components/admin/gestion-signalement/gestion-signalement.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { AdminprofilService } from '../../../Services/admin-dashboard-service';
 
 @Component({
   selector: 'app-gestion-signalement',
@@ -7,4 +8,30 @@ import { RouterLink } from "@angular/router";
   templateUrl: './gestion-signalement.html',
   styleUrl: './gestion-signalement.css',
 })
-export class GestionSignalement {}
+export class GestionSignalement implements OnInit{
+  incidentService = inject(AdminprofilService);
+  signalements = this.incidentService.listeSignalements;
+  loading = signal(false);
+  errorMessage = signal('');
+
+  ngOnInit(): void {
+    this.chargerSignalement();
+  }
+
+  chargerSignalement(){
+    this.loading.set(true);
+    this.errorMessage.set('');
+
+    this.incidentService.incidents().subscribe({
+      next: (signalement) =>{
+        console.log("signalements recuperer", signalement);
+      },
+      error: (error) => {
+        console.log("erreur API", error);
+      }
+    })
+  }
+
+
+
+}
