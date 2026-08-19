7777809cf32721bc33cf930d83e12bb638fac9a6
M_DIONE <admin@192.168.1.31>
2026-08-19 16:36:49 +0000

    merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -52,11 +52,7 @@ export const routes: Routes = [
   {
     path: 'admin', component: Layout,
     children: [
-      { path: 'dashboard', component: Dashboard },
-      { path: 'signalements', component: GestionSignalement},
       { path: 'signalements/detail/:id', component: SignalementDetailAdmin},
-      { path: 'statistiques', component: AdminAnalyse},
-      { path: 'parametres', component: AdminSecurite},
       { path: 'dashboard', component: Dashboard,
          canActivate: [authGuardAdmin, roleGuardAdmin], data: { roles: ['admin']}
        },
