5f9701c769557e11ec235aa1af9b41bbdbcc467e
Aissata-Sow788 <saissata676@gmail.com>
2026-08-19 01:14:04 +0000

    Affichage de carte map et les graphique

--- src/app/Models/incident.model.ts
+++ src/app/Models/incident.model.ts
@@ -17,4 +17,6 @@ export interface Incident {
   dateModification: string;
   priorite: string;
   urlImage: string;
+  type_incident: string | null; // Ajout du champ provenant du Serializer Django
+  citoyen?: number; // Optionnel : si vous souhaitez aussi manipuler l'ID du citoyen plus tard
 }
