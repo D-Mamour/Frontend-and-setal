e09aa4b7c223cf8b6ad804fa6bd5b81bf2ce2c34
Fatoumata DRAME <dramefatoumata029@gmail.com>
2026-08-19 19:19:11 +0000

    Implementation de la page carte de proximite

--- src/app/components/agent/signalement-map/signalement-map.ts
+++ src/app/components/agent/signalement-map/signalement-map.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Navbar } from "../../navbar/navbar";
+import { GeolocalisationError, GeolocalisationService } from '../../../Services/geolocalisation.service';
 
 @Component({
   selector: 'app-signalement-map',
